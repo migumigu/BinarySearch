f64b299a820f9653c868d5f25ad8bb8e555a60af
杨怀志 <aidedaijiayang@126.com>
2014-02-10 21:52:52 +0800

    create
    大学时代码

---
+++ Ordered_list.h
@@ -0,0 +1,8 @@
+#include"Record.h"
+#include"List.cpp"
+class Ordered_list: public List<Record>{
+public:
+	Error_code insert(const Record &data);
+	Error_code insert(int position, const Record &data);
+	Error_code replace(int position, const Record &data);
+};
